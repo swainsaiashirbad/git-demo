f5c9283744260cb984b58bef32d54381e01adc0b
saiAshirbad <swainsaiashirbad@gmail.com>
2024-10-09 17:54:45 +0530

    i printed my name

---
+++ git/src/main/java/git/example/GitApplication.java
@@ -0,0 +1,15 @@
+package git.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitApplication.class, args);
+		
+	System.out.println("sai aashirbad");
+	}
+
+}
