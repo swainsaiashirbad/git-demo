502127fe641423ae83a6f808c6d7e71fcc364b3f
saiAshirbad <swainsaiashirbad@gmail.com>
2024-10-10 16:48:50 +0530

    controler method added

--- git/src/main/java/git/example/controler/UserControler.java
+++ git/src/main/java/git/example/controler/UserControler.java
@@ -1,5 +1,13 @@
 package git.example.controler;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserControler {
 
+	@GetMapping("getNAme")
+	public String getNAme() {
+		return "sai";
+	}
 }
